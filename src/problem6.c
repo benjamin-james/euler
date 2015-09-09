d1db411e3039f6b6568714ecc1831f825e6ff7b6
Benjamin James <benjamin-james@utulsa.edu>
2015-09-09 11:29:03 -0500

    problem 6

---
+++ src/problem6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(void)
+{
+	uint64_t sum = 0;
+	uint64_t squares = 0;
+	uint64_t u;
+	for (u = 1; u <= 100; u++) {
+		sum += u;
+		squares += u * u;
+	}
+	u = sum * sum - squares;
+	printf("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is %" PRIu64 "\n", u);
+	return 0;
+}
